4fb906a82b7af354c2a109bd6b3966c48f480208
twj2 <brianteh007@gmail.com>
2024-05-05 13:58:48 +0800

    updated websocket

--- index.js
+++ index.js
@@ -2,15 +2,22 @@
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const cors = require('cors');
 
 // Create a new Express application
 const app = express();
+// Enable CORS with specific origin(s)
+//app.use(cors());
 
 // Create a basic HTTP server using Express
 const server = http.createServer(app);
 
 // Create a new instance of Socket.IO by passing the HTTP server
-const io = socketIO(server);
+const io = socketIO(server, {
+  cors: {
+    origin: ['http://localhost','http://eliteolympiad.localdev','http://portal.eliteolympiad.org']
+  }}
+);
 
 // Define a connection event handler
 io.on('connection', (socket) => {
